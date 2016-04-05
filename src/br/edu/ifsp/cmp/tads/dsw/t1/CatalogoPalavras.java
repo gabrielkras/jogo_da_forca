fe29ff3492f3adef22b0111914b0ee14e4922c26
Daniel Teixeira <daniel.t.dt@gmail.com>
2016-04-05 20:27:26 -0300

    + catalogo de palavras

--- src/br/edu/ifsp/cmp/tads/dsw/t1/CatalogoPalavras.java
+++ src/br/edu/ifsp/cmp/tads/dsw/t1/CatalogoPalavras.java
@@ -1,7 +1,34 @@
 package br.edu.ifsp.cmp.tads.dsw.t1;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class CatalogoPalavras {
 	private List<String> palavras;
+	
+	public CatalogoPalavras() {
+		palavras = new ArrayList<>();
+	}
+	
+	public String getPalavra() {
+		String palavra = palavras.get(new Random().nextInt(palavras.size()));
+		
+		palavras.remove(palavra);
+		
+		return palavra; 
+	}
+	
+	public boolean addPalavra(String str) {
+		String palavra = str.toString().trim().toLowerCase();
+		
+		if (palavras.contains(palavra)) return false;
+		
+		palavras.add(palavra);
+		return true;
+	}
+	
+	public void limparCatalogo() {
+		palavras.clear();
+	}
 }
